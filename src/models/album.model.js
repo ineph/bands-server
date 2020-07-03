d34bb9327c16dd601454e15a4b7b0a6a8b48081c
lucas <lcs999999999@hotmail.com>
2020-07-03 17:47:59 -0300

    album model

---
+++ src/models/album.model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const AlbumSchema = new Schema({
+    name:         {type: String, required: true},
+    songs:        {type: String, required: true},
+    bands:        {type: String, required: true},
+    release_year: {type: String},
+    type:         {type: String},
+    format:       {type: String},
+    lineup:       {type: String}
+}, {
+    timestamps: true
+});
+
+const AlbumSchema = mongoose.model('Album', AlbumSchema);
+module.exports = AlbumSchema;
