11080b83697a7d24b954ac5691ee2f8ceb7f988a
lucas <lcs999999999@hotmail.com>
2020-07-07 22:30:11 -0300

    added basic CRUD for artist and song

---
+++ src/routes/artist.router.js
@@ -0,0 +1,62 @@
+const router = require('express').Router();
+const Artist = require('../models/artist.model');
+
+//get all
+router.route('/').get((req, res) => {
+    Artist.find()
+    .then(artists => res.json(artists))
+    .catch(err => res.status(400).json('Error: ' + err))
+});
+
+//get specific
+router.route('/:id').get((req, res) => {
+    Artist.findById(req.params.id)
+    .then(artist => res.json(artist))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+//post new one
+router.route('/add').post((req, res) => {
+
+    const age = req.body.age
+    const bands = req.body.bands
+    const songs = req.body.songs
+    const albums = req.body.albums
+    const gender = req.body.gender
+    const nickname = req.body.nickname 
+    const biography = req.body.biography
+    const full_name = req.body.full_name
+    const place_of_origin = req.body.place_of_origin
+
+    const newArtist = new Artist({
+        age,
+        bands,
+        songs,
+        albums,
+        gender,
+        nickname,
+        biography,
+        full_name,
+        place_of_origin
+    })
+
+    newArtist.save()
+    .then(() => res.json('Artist added! ID: ' + newArtist._id))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+//update sigle one
+router.route('/update/:id').patch((req, res) => {
+    Artist.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    .then(updatedArtist => res.json('Updated: ' + updatedArtist))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+//delete one
+router.route('/:id').delete((req, res) => {
+    Artist.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Artist ' + req.params.id + ' deleted successfully'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+module.exports = router;
---
+++ src/routes/song.router.js
@@ -0,0 +1,53 @@
+const router = require('express').Router();
+const Song = require('../models/song.model');
+
+//get all
+router.route('/').get((req, res) => {
+    Song.find()
+    .then(songs => res.json(songs))
+    .catch(err => res.status(400).json('Error: ' + err))
+});
+
+//get specific
+router.route('/:id').get((req,res) => {
+    Song.findById(req.params.id)
+    .then(song => res.json(song))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+//post new one
+router.route('/add').post((req, res) => {
+
+    const name = req.body.name
+    const bands = req.body.bands
+    const albums = req.body.albums
+    const lyrics = req.body.lyrics
+    const lineup = req.body.lineup
+
+    const newSong = new Song({
+        name,   
+        bands,  
+        albums, 
+        lyrics, 
+        lineup
+    })
+
+    newSong.save()
+    .then(() => res.json('New song added! ID: ' + newSong._id))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+//update single one
+router.route('/update/:id').patch((req, res) => {
+    Song.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    .then(updatedSong => res.json('Song updated: ' + updatedSong))
+});
+
+//delete one
+router.route('/:id').delete((req, res) => {
+    Song.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Deleted successfully'))
+    .catch(err => res.status(400).json('Error: ' + err))
+});
+
+module.exports = router;
