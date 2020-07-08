3733a1e562343bcbf126ace8faa31c67f14e0f5d
lucas <lcs999999999@hotmail.com>
2020-07-07 22:29:18 -0300

    ordered values on Band object

--- src/routes/band.router.js
+++ src/routes/band.router.js
@@ -11,8 +11,8 @@ router.route('/').get((req, res) => {
 //get specific
 router.route('/:id').get((req, res) => {
     Band.findById(req.params.id)
-    .then(band => {res.json(band)})
-    .catch(err => {res.status(400).json('Error: ' + err)})
+    .then(band => res.json(band))
+    .catch(err => res.status(400).json('Error: ' + err))
 });
 
 //post new one
@@ -20,24 +20,24 @@ router.route('/add').post((req, res) => {
     
     const name = req.body.name;
     const genres = req.body.genres;
-    const lyrical_themes = req.body.lyrical_themes;
-    const country_of_origin = req.body.country_of_origin;
-    const location = req.body.location;
+    const labels = req.body.labels;
     const status = req.body.status;
+    const location = req.body.location;
     const formed_in = req.body.formed_in;
     const years_active = req.body.years_active;
-    const labels = req.body.labels;
+    const lyrical_themes = req.body.lyrical_themes;
+    const country_of_origin = req.body.country_of_origin;
     
     const newBand = new Band({
         name,
         genres,
-        lyrical_themes,
-        country_of_origin,
-        location,
+        labels,
         status,
+        location,
         formed_in,
         years_active,
-        labels
+        lyrical_themes,
+        country_of_origin
     });
     
     newBand.save()
@@ -48,8 +48,8 @@ router.route('/add').post((req, res) => {
 //update sigle one
 router.route('/update/:id').patch((req, res) => {
     Band.findByIdAndUpdate(req.params.id, req.body, {new: true})
-    .then(updatedBand => {res.json('Updated: ' + updatedBand)})
-    .catch(err => {res.status(400).json('Error: '+ err)})
+    .then(updatedBand => res.json('Updated: ' + updatedBand))
+    .catch(err => res.status(400).json('Error: '+ err))
 });
 
 //delete one
